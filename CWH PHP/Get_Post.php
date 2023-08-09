1d3af4e93b4a96f5204ffa890dfafab0bf5612e2
hassan2112d <hassan2112d@aptechgdn.net>
2023-08-10 04:17:17 +0500

    hello

---
+++ CWH PHP/Get_Post.php
@@ -0,0 +1,82 @@
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Get post</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+      <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+          <a class="navbar-brand" href="#">Get/Post</a>
+          <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+              aria-expanded="false" aria-label="Toggle navigation"></button>
+          <div class="collapse navbar-collapse" id="collapsibleNavId">
+              <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                  <li class="nav-item active">
+                      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="#">Link</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                      <div class="dropdown-menu" aria-labelledby="dropdownId">
+                          <a class="dropdown-item" href="#">Action 1</a>
+                          <a class="dropdown-item" href="#">Action 2</a>
+                      </div>
+                  </li>
+              </ul>
+              <form class="form-inline my-2 my-lg-0">
+                  <input class="form-control mr-sm-2" type="text" placeholder="Search">
+                  <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+              </form>
+          </div>
+      </nav>
+      <?php
+      
+      if ($_SERVER['REQUEST_METHOD'] == "POST")
+      {
+          $email = $_POST['email'];
+          $password = $_POST['password'];
+
+          echo '  <div class="alert alert-success alert-dismissible fade show" role="alert"><p>Your email '.$email.' and password '.$password.' is submitted successfully</p> <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button></div>
+          
+          ';
+      }
+      else{
+        echo '<h1 class="text-center">Please submit below form</h1>';
+      }
+      
+      
+      ?>
+      
+      <div class="container mt-5">
+       <form action="/php//CWH PHP/Get_Post.php" method="Post">
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email">
+              
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" name="password">
+            </div>
+            
+            <button type="submit" class="btn btn-dark">Submit</button>
+        </form>
+      </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ CWH PHP/sqlconnect.php
@@ -0,0 +1,29 @@
+<?php
+
+echo 'welcome to connection of mysql with php';
+
+// Two ways to connect databse
+//1. Mysqli (procedure, OOP)
+//2.PDO
+
+//Procedure
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+
+$conn1 = mysqli_connect($servername,$username,$password);
+
+if(!$conn1)
+{
+    echo "sorry you are not connected ".mysqli_connect_error();
+}
+else{
+
+
+   echo "<br> you are connected successfully";
+
+}
+
+?>
