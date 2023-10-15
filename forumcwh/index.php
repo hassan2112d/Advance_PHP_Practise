013add229b6555583a8435f1841ef3aef90bdd49
hassan2112d <hassan2112d@aptechgdn.net>
2023-10-15 20:41:05 +0500

    hello

--- forumcwh/index.php
+++ forumcwh/index.php
@@ -12,9 +12,21 @@
   </head>
   <body>
         <?php include 'partials/header.php';  ?>
-         <div class="container mt-4">
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id, fugiat. Quod saepe incidunt aut harum. Neque qui cupiditate consequuntur, est fuga inventore laudantium tempore. Vitae doloremque possimus reiciendis voluptatum aliquam id aut! Magnam tempore, fuga placeat saepe minima nobis inventore nihil? Nihil qui nam quibusdam ab? Praesentium mollitia dicta aliquam laborum, nobis modi dolore soluta voluptates hic exercitationem, velit animi, beatae aspernatur maxime temporibus aliquid nostrum eveniet odit reiciendis amet minus sunt cupiditate obcaecati? Tempore, minima amet, animi maxime inventore labore neque optio, assumenda magni commodi fugiat? Necessitatibus delectus natus architecto nostrum maiores deserunt, minima, molestiae itaque officia voluptatem enim tempore tempora. Doloribus, ad rem, tenetur accusantium laborum consectetur culpa explicabo in cupiditate molestias laboriosam veritatis quis voluptas quos id, cumque inventore assumenda magni eos et eveniet voluptatem! Saepe ad eligendi, voluptates perspiciatis illo reprehenderit nobis repellendus dolorem voluptatum corrupti fugit quaerat quod suscipit possimus deserunt, optio rerum accusantium perferendis culpa delectus est aperiam officia odit. Perferendis dolore doloremque nemo earum quos illum cumque. Fuga vel, saepe veniam quisquam inventore, suscipit repellendus placeat necessitatibus ut, temporibus obcaecati quibusdam est? Ducimus, praesentium deleniti asperiores obcaecati magnam omnis placeat, harum tenetur modi similique quia voluptate at odio sint eaque pariatur porro iste perferendis in ipsam quidem. Quae illum obcaecati eligendi sequi eos autem culpa impedit ratione fugiat quaerat porro natus earum placeat odio iste, quasi nemo vitae similique. Corporis sequi aliquam amet iure inventore tempore in quam. Non nihil deleniti sequi. Itaque cupiditate mollitia repudiandae, id ullam, non libero animi culpa nihil tempora reiciendis! Accusamus pariatur voluptate fugiat corrupti id neque ut harum qui placeat dignissimos ipsum, nemo aut impedit molestiae! Debitis, quo. Nulla, eveniet totam? Fugit quidem placeat quibusdam quos ducimus. Omnis sapiente ipsa pariatur quam dolorum, totam quisquam ut quasi commodi, fuga, blanditiis qui enim nam in doloribus. Cum dolores, id numquam enim pariatur doloribus totam non excepturi assumenda, tenetur ipsam sint impedit error ipsa quae iste ab, officia facere? Quam, adipisci? Doloribus cumque doloremque alias est iusto quisquam, corrupti libero optio nobis quaerat quia veritatis pariatur non dolor beatae! In magnam itaque dolorum sit, neque deserunt suscipit ratione dignissimos. Laborum eos quos et nostrum tempore! Rerum molestias suscipit provident adipisci fuga omnis, repellat error impedit! Neque veniam incidunt et maxime unde dolorem voluptates perferendis! Commodi accusamus consequatur unde, fugiat corporis numquam quibusdam minus repudiandae ratione beatae maiores ipsa. Voluptates voluptatem tempore perspiciatis id, ex, repellat recusandae odio vitae, minima architecto minus quisquam ratione et obcaecati commodi atque. Id sequi commodi reprehenderit expedita corporis a ea non? Optio porro, maiores sit earum nisi possimus. Quas est iste accusamus veniam sint cupiditate, recusandae repellat nihil perspiciatis exercitationem dolore harum beatae nulla in voluptatem asperiores soluta maxime sequi nemo repellendus. Ut illo asperiores vero sapiente deleniti, eos ex!</p>
-         </div>
+          <div class="container mt-4">
+            <h2 class="text-center">CODEIX -- Coding Platform</h2>
+            <div class="row">
+              <div class="col-md-4 my-2">
+                <div class="card my-2" style="width: 18rem;">
+                  <img class="card-img-top" src="https://source.unsplash.com/500x400/?code,python" alt="Card image cap">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary ">View</a>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
         <?php include 'partials/footer.php'; ?>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
