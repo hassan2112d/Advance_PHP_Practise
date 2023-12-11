7534ea3e495d5b2a378143b539492dd15646fe2d
hassan2112d <hassan2112d@aptechgdn.net>
2023-12-12 03:27:54 +0500

    hello

--- forumcwh/partials/header.php
+++ forumcwh/partials/header.php
@@ -1,6 +1,19 @@
 <?php 
 
 echo '
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>Codeix - Coding Platform</title>
+  </head>
+<body>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 <a class="navbar-brand" href="/php/forumcwh">CODEIX</a>
---
+++ forumcwh/threadlist.php
@@ -0,0 +1,55 @@
+<?php include 'partials/_dbconnect.php';
+      include 'partials/header.php';
+      include 'partials/_signup.php';
+      include 'partials/_login.php';
+      
+?>
+<div class="container mt-4">
+    <div class="jumbotron">
+        <h1 class="display-4">Welcome to PHP threads</h1>
+        <p class="lead">PHP (Hypertext Processor) is a general-purpose scripting language and interpreter that is freely available and widely used for web development. The language is used primarily for server-side scripting, although it can also be used for command-line scripting and, to a limited degree, desktop applications.</p>
+        <hr class="my-4">
+        <p>(IMPORTANT) Guidelines for Forum Posts Participate in online forums as you would in constructive, face-to-face discussions.Postings should continue a conversation and provide avenues for additional continuous dialogue. Do not post “I agree,” or similar, statements.Stay on the topic of the thread – do not stray.</p>
+        <p class="lead">
+            <a class="btn btn-danger btn-lg" href="#" role="button">Learn more</a>
+        </p>
+    </div>
+        <h2 class="mt-4 mb-4 py-4 ">QUESTIONS :</h2>
+    <div class="media mb-4">
+        <img class="mr-3" src="https://source.unsplash.com/500x400/?user,defaultimage" width="70px" height="70px" alt="Generic placeholder image">
+        <div class="media-body">
+            <h5 class="mt-0">Issue in Python</h5>
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+        </div>
+    </div>
+    <div class="media mb-4">
+        <img class="mr-3" src="https://source.unsplash.com/500x400/?user,defaulticon" width="70px" height="70px" alt="Generic placeholder image">
+        <div class="media-body">
+            <h5 class="mt-0">Media heading</h5>
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+        </div>
+    </div>
+    <div class="media mb-4">
+        <img class="mr-3" src="https://source.unsplash.com/500x400/?user,humanimage" width="70px" height="70px" alt="Generic placeholder image">
+        <div class="media-body">
+            <h5 class="mt-0">Media heading</h5>
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+        </div>
+    </div>
+    <div class="media mb-4">
+        <img class="mr-3" src="https://source.unsplash.com/500x400/?user,icon" width="70px" height="70px" alt="Generic placeholder image">
+        <div class="media-body">
+            <h5 class="mt-0">Media heading</h5>
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+        </div>
+    </div>
+    <div class="media mb-4">
+        <img class="mr-3" src="https://source.unsplash.com/500x400/?user,human" width="70px" height="70px" alt="Generic placeholder image">
+        <div class="media-body">
+            <h5 class="mt-0">Media heading</h5>
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+        </div>
+    </div>
+</div>
+
+<?php  include 'partials/footer.php'; ?>
