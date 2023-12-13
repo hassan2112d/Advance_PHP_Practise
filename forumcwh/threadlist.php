8dc83e333b65d01ea3b05769b2f4dd95b6a18dbc
hassan2112d <hassan2112d@aptechgdn.net>
2023-12-14 03:54:56 +0500

    hello world

--- forumcwh/threadlist.php
+++ forumcwh/threadlist.php
@@ -18,6 +18,28 @@ while($row = mysqli_fetch_assoc($result))
     $categorydesc = $row['category_description'];
 }
 
+$method = $_SERVER['REQUEST_METHOD'];
+$showalert = false;
+if($method == "POST"){
+    $showalert = true;
+    $title = $_POST['title'];
+    $desc = $_POST['desc'];
+
+     $sql= "INSERT INTO `thread` (`thread_name`, `thread_desc`, `thread_cat_id`, `thread_user_id`, `added_on`)
+     VALUES ('$title', '$desc', '$id', '0', current_timestamp())";
+
+     $result = mysqli_query($conn,$sql);
+
+     if($showalert){
+        echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+                <strong>SUCCESS !</strong> You Question submitted Successfully.
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>';
+     }
+
+}
 ?>
 <div class="container mt-4">
     <div class="jumbotron">
@@ -29,18 +51,33 @@ while($row = mysqli_fetch_assoc($result))
             <a class="btn btn-danger btn-lg" href="#" role="button">Learn more</a>
         </p>
     </div>
-
+    <div class="container">
+        <h2 class="mt-4 mb-4 py-4 ">Start Disscussion:</h2>
+        <form action="<?php $_SERVER['REQUEST_URI'] ?>" method="post">
+            <div class="form-group">
+                <label for="title">Problem Title:</label>
+                <input type="text" class="form-control" id="exampleInputEmail1" name="title" aria-describedby="emailHelp">
+                <small id="emailHelp" class="form-text text-muted">Keep title in short.</small>
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlTextarea1">Problem Description:</label>
+                <textarea class="form-control" name="desc" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+        
+            <button type="submit" class="btn btn-success">Submit</button>
+        </form>
+    </div>
     <div class="container" id="ques" >
          <h2 class="mt-4 mb-4 py-4 ">QUESTIONS :</h2>
 
-    <?php 
+<?php 
 
        $id = $_GET['catid'];
        $sql = "SELECT * FROM `thread` WHERE thread_cat_id = $id";
        $result = mysqli_query($conn,$sql);
-       
+       $noresult = true;
        while($row = mysqli_fetch_assoc($result)){
-
+        $noresult = false;
         $id = $row['thread_id'];
         $name = $row['thread_name'];
         $desc = $row['thread_desc'];
@@ -55,8 +92,20 @@ while($row = mysqli_fetch_assoc($result))
         </div>';
         
        }
+       
+       if($noresult){
+        
+         echo '<div class="jumbotron jumbotron-fluid">
+         <div class="container">
+           <h1 class="display-4">No Question</h1>
+           <p class="lead">Be the first one to ask the questions</p>
+         </div>
+       </div>';
+       }
 
-    ?>
+ ?>
+
+
 <!--     
     <div class="media mb-4">
         <img class="mr-3" src="https://source.unsplash.com/500x400/?user,human" width="70px" height="70px" alt="Generic placeholder image">
